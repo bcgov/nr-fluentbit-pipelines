97404cea89ded531a420f25c14e4b0e914be0163
Matthew Bystedt <matthew.bystedt@gov.bc.ca>
2022-03-18 13:55:36 -0700

    fix: output dir

--- scripts/deploy_config.sh
+++ scripts/deploy_config.sh
@@ -4,7 +4,7 @@ sshpass -p $CD_PASS scp -q -r files $CD_USER@$HOST:
 sshpass -p $CD_PASS scp -q -r $FUNBUCKS_OUTPUT $CD_USER@$HOST:
 sshpass -p $CD_PASS ssh -q $CD_USER@$HOST /bin/bash <<EOF
 sudo -su wwwadm
-cp -R $FUNBUCKS_OUTPUT/* $AGENT_HOME/conf
+cp -R output/* $AGENT_HOME/conf
 cp files/fluent-bit.hcl $AGENT_HOME/conf
 cp files/fluentbitw $AGENT_HOME/bin
 cp files/fluent-bit-logrotate.conf ~/
