ba32a7887e8ed6960b76fc9e331d36c5e0870ef4
Matthew Bystedt <matthew.bystedt@gov.bc.ca>
2022-03-28 13:21:08 -0700

    feat: switch to jobs

--- scripts/deploy_agents.sh
+++ scripts/deploy_agents.sh
@@ -53,11 +53,9 @@ for agent in \${AGENTS[@]} ; do
     cp -R $TMP_DIR/output/\$AGENT/* \$AGENT_HOME/conf
     sed -e "s,\\\$HTTP_PROXY,\$HTTP_PROXY,g" -e "s,{{ apm_agent_home }},\$AGENT_HOME,g" $TMP_DIR/files/fluent-bit.hcl > \$AGENT_HOME/conf/fluent-bit.hcl
     cp $TMP_DIR/files/fluentbitw \$AGENT_HOME/bin
+    cp $TMP_DIR/files/.env \$AGENT_HOME/bin/.env
     sed "s,{{ apm_agent_home }},\$AGENT_HOME,g" $TMP_DIR/files/fluent-bit-logrotate.conf > \$AGENT_HOME/\$AGENT-logrotate.conf
     ln -sfn \$AGENT_HOME/bin/fluentbitw $S6_SERVICE_HOME/\$AGENT/run
-    # TODO: revoke previous token
-    # deploy new token to .env file
-    sed 's/VAULT_TOKEN=.*/VAULT_TOKEN="${APP_TOKEN}"/g' $TMP_DIR/files/.env > \$AGENT_HOME/bin/.env
     chmod 775 \$AGENT_HOME/bin/fluentbitw \$AGENT_HOME/db \$AGENT_HOME/logs
 done
 EOF
--- scripts/start_fluentbit.sh
+++ scripts/start_fluentbit.sh
@@ -7,6 +7,10 @@ sudo -su wwwsvr
 AGENTS=\$(ls -d $AGENT_ROOT/fluent-bit.*)
 for agent in \${AGENTS[@]} ; do
     AGENT=\$(basename \$agent)
+    AGENT_HOME=$AGENT_ROOT/\$AGENT
+    # TODO: revoke previous token
+    # deploy new token to .env file
+    sed -i 's/VAULT_TOKEN=.*/VAULT_TOKEN="${APP_TOKEN}"/g' \$AGENT_HOME/bin/.env
     if [ -r $S6_SERVICE_HOME/\$AGENT/run ]; then
         /sw_ux/s6/bin/s6-svc -u $S6_SERVICE_HOME/\$AGENT/
     fi
