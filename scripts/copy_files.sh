a060d2259b2d8057362d638fb58eeec49e89a77c
Matthew Bystedt <matthew.bystedt@gov.bc.ca>
2022-04-01 12:19:57 -0700

    fix: permissions

--- scripts/copy_files.sh
+++ scripts/copy_files.sh
@@ -11,3 +11,6 @@ chmod -R 775 $TMP_DIR
 EOF
 sshpass -p $CD_PASS scp -q -r files $CD_USER@$HOST:$TMP_DIR
 sshpass -p $CD_PASS scp -q -r $FUNBUCKS_OUTPUT $CD_USER@$HOST:$TMP_DIR
+sshpass -p $CD_PASS ssh -q $CD_USER@$HOST /bin/bash <<EOF
+chmod -R 775 $TMP_DIR
+EOF
