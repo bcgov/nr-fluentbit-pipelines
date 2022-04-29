b988fafb309e66069358993bf671a187de1c4dd0
Andreas Wilson <Andreas.Wilson@gov.bc.ca>
2022-04-29 16:07:47 -0700

    feat: change policy for token gen

--- scripts/setenv.sh
+++ scripts/setenv.sh
@@ -1,6 +1,6 @@
 export VAULT_ADDR=https://vault-iit.apps.silver.devops.gov.bc.ca
 export VAULT_TOKEN=$(vault login -method=oidc -format json | jq -r '.auth.client_token')
-export WRAPPING_TOKEN=$(vault token create -policy=auth/approle/role/broker \
+export WRAPPING_TOKEN=$(vault token create -policy=auth/vs_apps_approle/role/fluent_fluent-bit_prod/secret-id-write \
     -policy=shared/groups/appdelivery/jenkins-isss-cdua-read \
     -policy=shared/groups/appdelivery/jenkins-isss-ci-read \
     -policy=auth/vs_apps_approle/role/fluent_fluent-bit_prod/role-id-read \
