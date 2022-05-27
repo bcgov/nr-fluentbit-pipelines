8447a04fdc8a3acaed5ed5aee7fe4159daa71673
Matthew Bystedt <matthew.bystedt@gov.bc.ca>
2022-05-27 09:04:03 -0700

    feat: Detect main host network interface name

--- scripts/start_fluentbit.sh
+++ scripts/start_fluentbit.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 set +x
+
+SERVER_IP=$(dig +short $HOST | tail -n1)
+
 sshpass -p $CD_PASS ssh -q $CD_USER@$HOST /bin/bash <<EOF
 # become run_user
 if [ "$PCI" = "true" ]; then
@@ -11,6 +14,16 @@ fi
 # get role-id
 ROLE_ID=\$(set +x; VAULT_ADDR=$VAULT_ADDR VAULT_TOKEN=$VAULT_TOKEN /sw_ux/bin/vault read -field=role_id auth/vs_apps_approle/role/fluent_fluent-bit_prod/role-id)
 
+# extract interface name
+
+INTERFACE_NAME=\$(ip addr | awk '
+/^[0-9]+:/ {
+  sub(/:/,"",\$2); iface=\$2 }
+/^[[:space:]]*inet / {
+  split(\$2, a, "/")
+  print iface" : "a[1]
+}' | grep $SERVER_IP | sed 's/\\s.*//g')
+
 AGENTS=\$(ls -d $AGENT_ROOT/fluent-bit.*)
 for agent in \${AGENTS[@]} ; do
     AGENT=\$(basename \$agent)
@@ -21,11 +34,11 @@ for agent in \${AGENTS[@]} ; do
         PREVIOUS_TOKEN=\$(cat \$AGENT_HOME/bin/.env | grep VAULT_TOKEN | awk -F '"' '{print \$2}')
         VAULT_ADDR=$VAULT_ADDR VAULT_TOKEN=\$PREVIOUS_TOKEN /sw_ux/bin/vault token revoke -self
     fi
-    # generate and deploy new app token to .env file    
+    # generate and deploy new app token to .env file
     WRAPPING_TOKEN=\$(set +x; VAULT_ADDR=$VAULT_ADDR VAULT_TOKEN=$VAULT_TOKEN /sw_ux/bin/vault write -wrap-ttl=120s -f -field=wrapping_token auth/vs_apps_approle/role/fluent_fluent-bit_prod/secret-id)
     SECRET_ID=\$(set +x; VAULT_ADDR=$VAULT_ADDR VAULT_TOKEN=\$WRAPPING_TOKEN /sw_ux/bin/vault unwrap -field=secret_id)
     APP_TOKEN=\$(set +x; VAULT_ADDR=$VAULT_ADDR VAULT_TOKEN=$VAULT_TOKEN /sw_ux/bin/vault write -force -field=token auth/vs_apps_approle/login role_id=\$ROLE_ID secret_id=\$SECRET_ID)
-    sed 's/VAULT_TOKEN=.*/'VAULT_TOKEN=\""\$APP_TOKEN"\"'/g' \$AGENT_HOME/bin/.env.template > \$AGENT_HOME/bin/.env
+    sed 's/VAULT_TOKEN=.*/'VAULT_TOKEN=\""\$APP_TOKEN"\"'/g;s/METRIC_HOST_NETWORK_INTERFACE_NAME=.*/'METRIC_HOST_NETWORK_INTERFACE_NAME=\""\$METRIC_HOST_NETWORK_INTERFACE_NAME"\"'/g' \$AGENT_HOME/bin/.env.template > \$AGENT_HOME/bin/.env
     chmod 700 \$AGENT_HOME/bin/.env
     if [ -r $S6_SERVICE_HOME/\$AGENT/run ]; then
         /sw_ux/s6/bin/s6-svc -u $S6_SERVICE_HOME/\$AGENT/
