b3e2b1bb0b1d8cb958738dd11a73081f81abd0dc
Matthew Bystedt <matthew.bystedt@gov.bc.ca>
2022-06-15 16:39:53 -0700

    feat: prototype automation pipeline

---
+++ scripts/automation_setup.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set +x
+
+if [ -f "workflow-cli.zip" ]; then
+    echo "CLI exists. Clear workspace to redownload workflow cli."
+else
+  /bin/curl -u $CI_USER:$CI_PASS -sSL "http://bwa.nrs.gov.bc.ca/int/artifactory/ext-binaries-local/automation/workflow/1.0.0/workflow-cli.zip" -o workflow-cli.zip
+  unzip workflow-cli.zip
+fi
