918a1be4788c07b858e36cc753f9078c0f14f426
Matthew Bystedt <matthew.bystedt@gov.bc.ca>
2022-06-16 08:45:51 -0700

    feat: automation pipeline work

--- scripts/automation_setup.sh
+++ scripts/automation_setup.sh
@@ -4,6 +4,6 @@ set +x
 if [ -f "workflow-cli.zip" ]; then
     echo "CLI exists. Clear workspace to redownload workflow cli."
 else
-  /bin/curl -u $CI_USER:$CI_PASS -sSL "http://bwa.nrs.gov.bc.ca/int/artifactory/ext-binaries-local/automation/workflow/1.0.0/workflow-cli.zip" -o workflow-cli.zip
+  /bin/curl -u $PIPELINE_ARTIFACTORY_CREDS_USR:$PIPELINE_ARTIFACTORY_CREDS_PSW -sSL "http://bwa.nrs.gov.bc.ca/int/artifactory/ext-binaries-local/automation/workflow/1.0.0/workflow-cli.zip" -o workflow-cli.zip
   unzip workflow-cli.zip
 fi
