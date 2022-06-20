11c038537276daec5e2584c8b9b97385d8b728f8
Matthew Bystedt <matthew.bystedt@gov.bc.ca>
2022-06-20 14:53:01 -0700

    feat: rewrite vault commands using broker

--- scripts/setenv.sh
+++ scripts/setenv.sh
@@ -1,8 +1,8 @@
 export VAULT_ADDR=https://vault-iit.apps.silver.devops.gov.bc.ca
 export VAULT_TOKEN=$(vault login -method=oidc -format json | jq -r '.auth.client_token')
 export WRAPPING_TOKEN=$(vault token create -policy=auth/vs_apps_approle/role/fluent_fluent-bit_prod/secret-id-write \
-    -policy=shared/groups/appdelivery/jenkins-isss-cdua-read \
-    -policy=shared/groups/appdelivery/jenkins-isss-ci-read \
+    -policy=system/isss-cdua-read \
+    -policy=system/isss-ci-read \
     -policy=auth/vs_apps_approle/role/fluent_fluent-bit_prod/role-id-read \
     -orphan \
     -explicit-max-ttl=300 \
