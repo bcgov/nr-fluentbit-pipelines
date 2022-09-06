9c2e12c3702f664cf0a3dcbb6017be795098a339
Matthew Bystedt <matthew.bystedt@gov.bc.ca>
2022-09-06 13:12:28 -0700

    fix: broker changes

--- scripts/vault_secret_id.sh
+++ scripts/vault_secret_id.sh
@@ -6,6 +6,7 @@ cat scripts/provision-fluentbit.json > $TEMP_FILE
 
 curl -s -X POST $BROKER_URL/provision/secret-id -H 'Content-Type: application/json' \
   -H 'X-Vault-Role-Id: '"$FB_ROLE_ID"'' -u "$BASIC_HTTP_USER:$BASIC_HTTP_PASSWORD" \
-  -d @$TEMP_FILE
+  -d @$TEMP_FILE | \
+  /sw_ux/bin/jq -r '.wrap_info.token'
 
 rm $TEMP_FILE
