e228d8decdc57609c35ccb30804eef40d06ca768
Matthew Bystedt <matthew.bystedt@gov.bc.ca>
2022-09-26 16:39:59 -0700

    feat: switch to intention api

--- scripts/vault_token.sh
+++ scripts/vault_token.sh
@@ -4,11 +4,16 @@
 TEMP_FILE=$(mktemp)-isss-jenkins-broker
 cat $1 | /sw_ux/bin/jq ".event.url=\"$BUILD_URL\" | .event.action=\"job-$JOB_BASE_NAME\"" > $TEMP_FILE
 
-WRAPPED_VAULT_TOKEN=$(curl -s -X POST $BROKER_URL/provision/token -H 'Content-Type: application/json' \
-  -H 'X-Vault-Role-Id: '"$CONFIG_ROLE_ID"'' -u "$BASIC_HTTP_USER:$BASIC_HTTP_PASSWORD" \
-  -d @$TEMP_FILE | \
+TOKEN=$(curl -s -X POST $BROKER_URL/intention/open -H 'Content-Type: application/json' -u "$BASIC_HTTP_USER:$BASIC_HTTP_PASSWORD" -d @$TEMP_FILE | jq -r '.token')
+
+WRAPPED_VAULT_TOKEN=$(curl -s -X POST $BROKER_URL/provision/token/self \
+  -H 'Content-Type: application/json' \
+  -H 'X-Vault-Role-Id: '"$CONFIG_ROLE_ID"'' -H 'X-Broker-Token: '"$TOKEN"'' | \
   /sw_ux/bin/jq -r '.wrap_info.token')
 
 rm $TEMP_FILE
 
 VAULT_ADDR=$VAULT_ADDR /sw_ux/bin/vault unwrap -field=token $WRAPPED_VAULT_TOKEN
+
+# Close intention
+curl -s -X POST $BROKER_URL/intention/close -H 'X-Broker-Token: '"$TOKEN"''
