d034548d456e0feb90029bcc519b9bcc94c2ce1e
Matthew Bystedt <matthew.bystedt@gov.bc.ca>
2022-10-05 16:54:54 -0700

    feat: use intention api

---
+++ scripts/broker_intention_open.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Workaround because <() does not work
+TEMP_FILE=$(mktemp)-isss-jenkins-broker
+cat $1 | /sw_ux/bin/jq ".event.url=\"$BUILD_URL\" | .event.action=\"job-$JOB_BASE_NAME\"" > $TEMP_FILE
+
+curl -s -X POST $BROKER_URL/intention/open \
+    -H 'Content-Type: application/json' \
+    -u "$BASIC_HTTP_USER:$BASIC_HTTP_PASSWORD" \
+    -d @$TEMP_FILE
--- scripts/vault_secret_id.sh
+++ scripts/vault_secret_id.sh
@@ -1,16 +1,10 @@
 #!/bin/sh
 
-# Workaround because <() does not work
-TEMP_FILE=$(mktemp)
-cat scripts/provision-fluentbit.json > $TEMP_FILE
+TOKEN=$(echo $INTENTION_JSON | /sw_ux/bin/jq -r '.intention.provision.token')
 
-TOKEN=$(curl -s -X POST $BROKER_URL/intention/open -H 'Content-Type: application/json' -u "$BASIC_HTTP_USER:$BASIC_HTTP_PASSWORD" -d @$TEMP_FILE | jq -r '.token')
-
-curl -s -X POST $BROKER_URL/provision/approle/secret-id -H 'Content-Type: application/json' \
+WRAPPED_VAULT_TOKEN=$(curl -s -X POST $BROKER_URL/provision/approle/secret-id \
+  -H 'Content-Type: application/json' \
   -H 'X-Vault-Role-Id: '"$FB_ROLE_ID"'' -H 'X-Broker-Token: '"$TOKEN"'' | \
-  /sw_ux/bin/jq -r '.wrap_info.token'
+  /sw_ux/bin/jq -r '.wrap_info.token')
 
-rm $TEMP_FILE
-
-# Close intention
-curl -s -X POST $BROKER_URL/intention/close -H 'X-Broker-Token: '"$TOKEN"''
+VAULT_ADDR=$VAULT_ADDR /sw_ux/bin/vault unwrap -field=token $WRAPPED_VAULT_TOKEN
