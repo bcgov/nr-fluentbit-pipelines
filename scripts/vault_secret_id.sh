5de9338aab207ac90b028dfde93d11e7219718b9
Matthew Bystedt <matthew.bystedt@gov.bc.ca>
2022-10-06 15:08:31 -0700

    fix: do not unwrap secret id

--- scripts/vault_secret_id.sh
+++ scripts/vault_secret_id.sh
@@ -2,9 +2,7 @@
 
 TOKEN=$(echo $INTENTION_JSON | /sw_ux/bin/jq -r '.actions.provision.token')
 
-WRAPPED_VAULT_TOKEN=$(curl -s -X POST $BROKER_URL/v1/provision/approle/secret-id \
+curl -s -X POST $BROKER_URL/v1/provision/approle/secret-id \
   -H 'Content-Type: application/json' \
   -H 'X-Vault-Role-Id: '"$FB_ROLE_ID"'' -H 'X-Broker-Token: '"$TOKEN"'' | \
-  /sw_ux/bin/jq -r '.wrap_info.token')
-
-VAULT_ADDR=$VAULT_ADDR /sw_ux/bin/vault unwrap -field=token $WRAPPED_VAULT_TOKEN
+  /sw_ux/bin/jq -r '.wrap_info.token'
