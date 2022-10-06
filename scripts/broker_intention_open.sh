d7e022914f7674a2bf08473d12df2800309f6c8e
Matthew Bystedt <matthew.bystedt@gov.bc.ca>
2022-10-06 14:59:13 -0700

    fix: remove action

--- scripts/broker_intention_open.sh
+++ scripts/broker_intention_open.sh
@@ -4,7 +4,6 @@
 TEMP_FILE=$(mktemp)-isss-jenkins-broker
 cat $1 | /sw_ux/bin/jq "\
     .event.url=\"$BUILD_URL\" | \
-    .event.action=\"job-$JOB_BASE_NAME\" | \
     (.actions[] | select(.id == \"install\") .service.version) |= \"$FLUENTBIT_RELEASE\" \
     " > $TEMP_FILE
 
