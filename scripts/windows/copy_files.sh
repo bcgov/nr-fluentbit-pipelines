630f3e3de2bbb72082da0ce32d7f2e928af55de9
Andreas Wilson <Andreas.Wilson@gov.bc.ca>
2023-05-11 15:29:11 -0700

    No host check

--- scripts/windows/copy_files.sh
+++ scripts/windows/copy_files.sh
@@ -5,5 +5,5 @@ New-Item -ItemType "directory" -Path "$FB_TMP_DIR" -Force
 New-Item -ItemType "directory" -Path "$FB_TMP_DIR/bin" -Force
 EOF
 
-sshpass -p $FB_CD_PASS scp -q -r files $FB_CD_USER@$FB_HOST:$FB_TMP_DIR
-sshpass -p $FB_CD_PASS scp -q -r $FB_FUNBUCKS_OUTPUT $FB_CD_USER@$FB_HOST:$FB_TMP_DIR
+sshpass -p $FB_CD_PASS scp -o 'StrictHostKeyChecking=no' -q -r files $FB_CD_USER@$FB_HOST:$FB_TMP_DIR
+sshpass -p $FB_CD_PASS scp -o 'StrictHostKeyChecking=no' -q -r $FB_FUNBUCKS_OUTPUT $FB_CD_USER@$FB_HOST:$FB_TMP_DIR
