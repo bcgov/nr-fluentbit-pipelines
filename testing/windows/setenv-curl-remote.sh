56b32078798988de2c360bce2d5db08354d33303
Matthew Bystedt <matthew.bystedt@gov.bc.ca>
2024-05-07 09:56:44 -0700

    chore: update prod broker url

--- testing/windows/setenv-curl-remote.sh
+++ testing/windows/setenv-curl-remote.sh
@@ -17,7 +17,7 @@ if [ -n "$1" ] && [ -n "$2" ]
   if [ "prod" = $2 ]
   then
     export VAULT_ADDR="https://knox.io.nrs.gov.bc.ca"
-    export FB_BROKER_URL="https://nr-broker.apps.silver.devops.gov.bc.ca"
+    export FB_BROKER_URL="https://broker.io.nrs.gov.bc.ca"
   fi
   VAULT_TOKEN=$(vault login -method=oidc -format json -tls-skip-verify | jq -r '.auth.client_token')
   export FB_CONFIG_ROLE_ID=$(vault read -format json auth/vs_apps_approle/role/jenkins_jenkins-isss_prod/role-id | jq -r '.data.role_id')
